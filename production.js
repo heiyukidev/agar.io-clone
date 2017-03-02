9b5e80eac0a3922c8fd00feb69bfb37ea79c156e
Khaled Romdhane <heiyukidev@gmail.com>
2017-03-02 16:58:10 +0100

    RAM inscrease

--- production.js
+++ production.js
@@ -1,7 +1,7 @@
 var pm2 = require('pm2');
 
 var instances = process.env.WEB_CONCURRENCY || 1;
-var maxMemory = process.env.WEB_MEMORY || 512;
+var maxMemory = process.env.WEB_MEMORY || 10240;
 
 pm2.connect(function() {
   pm2.start({
