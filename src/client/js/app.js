4b30deab7a9f8d0b01c9e0877e1bd2ed534b1d0e
Khaled Romdhane <heiyukidev@gmail.com>
2017-03-03 22:20:44 +0100

    optimizing Canvas

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -132,8 +132,6 @@ global.target = target;
 
 window.canvas = new Canvas();
 
-
-
 var c = window.canvas.cv;
 var graph = c.getContext('2d');
 
@@ -177,7 +175,6 @@ function setupSocket(socket) {
         global.player = player;
         socket.emit('gotit', player);
         global.gameStart = true;
-        debug('Game started at: ' + global.gameStart);
         if (global.mobile) {
             document.getElementById('gameAreaWrapper').removeChild(document.getElementById('chatbox'));
         }
@@ -476,51 +473,6 @@ function drawgrid() {
     graph.globalAlpha = 1;
 }
 
-function drawborder() {
-    graph.lineWidth = 1;
-    graph.strokeStyle = playerConfig.borderColor;
-
-    // Left-vertical.
-    if (player.x <= global.screenWidth / 2) {
-        graph.beginPath();
-        graph.moveTo(global.screenWidth / 2 - player.x, 0 ? player.y > global.screenHeight / 2 : global.screenHeight / 2 - player.y);
-        graph.lineTo(global.screenWidth / 2 - player.x, global.gameHeight + global.screenHeight / 2 - player.y);
-        graph.strokeStyle = global.lineColor;
-        graph.stroke();
-    }
-
-    // Top-horizontal.
-    if (player.y <= global.screenHeight / 2) {
-        graph.beginPath();
-        graph.moveTo(0 ? player.x > global.screenWidth / 2 : global.screenWidth / 2 - player.x, global.screenHeight / 2 - player.y);
-        graph.lineTo(global.gameWidth + global.screenWidth / 2 - player.x, global.screenHeight / 2 - player.y);
-        graph.strokeStyle = global.lineColor;
-        graph.stroke();
-    }
-
-    // Right-vertical.
-    if (global.gameWidth - player.x <= global.screenWidth / 2) {
-        graph.beginPath();
-        graph.moveTo(global.gameWidth + global.screenWidth / 2 - player.x,
-            global.screenHeight / 2 - player.y);
-        graph.lineTo(global.gameWidth + global.screenWidth / 2 - player.x,
-            global.gameHeight + global.screenHeight / 2 - player.y);
-        graph.strokeStyle = global.lineColor;
-        graph.stroke();
-    }
-
-    // Bottom-horizontal.
-    if (global.gameHeight - player.y <= global.screenHeight / 2) {
-        graph.beginPath();
-        graph.moveTo(global.gameWidth + global.screenWidth / 2 - player.x,
-            global.gameHeight + global.screenHeight / 2 - player.y);
-        graph.lineTo(global.screenWidth / 2 - player.x,
-            global.gameHeight + global.screenHeight / 2 - player.y);
-        graph.strokeStyle = global.lineColor;
-        graph.stroke();
-    }
-}
-
 window.requestAnimFrame = (function() {
     return window.requestAnimationFrame ||
         window.webkitRequestAnimationFrame ||
@@ -563,9 +515,6 @@ function gameLoop() {
             fireFood.forEach(drawFireFood);
             viruses.forEach(drawVirus);
 
-            if (global.borderDraw) {
-                drawborder();
-            }
             var orderMass = [];
             for (var i = 0; i < users.length; i++) {
                 for (var j = 0; j < users[i].cells.length; j++) {
