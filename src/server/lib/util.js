e2fbfa20b6f81fc28e7ee6f20c70efc5874dfeee
hamdij0maa <hamdijomaa.x@gmail.com>
2017-03-04 12:03:24 +0100

    svgfix

--- src/server/lib/util.js
+++ src/server/lib/util.js
@@ -84,6 +84,9 @@ exports.findIndex = function(arr, id) {
     return -1;
 };
 
+
+// we need to delete this if we need too :D
+
 exports.randomColor = function() {
     var color = '#' + ('00000' + (Math.random() * (1 << 24) | 0).toString(16)).slice(-6);
     var c = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(color);
