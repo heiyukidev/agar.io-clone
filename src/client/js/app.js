153d5a40ed6f1c914d0c00f15e3fb939725dbf00
hamdij0maa <hamdijomaa.x@gmail.com>
2017-03-04 15:32:30 +0100

    DrawGrid Change

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -463,12 +463,12 @@ function drawgrid() {
     graph.globalAlpha = 0.4;
     graph.beginPath();
 
-    for (var x = global.xoffset - player.x; x < global.screenWidth; x += global.screenHeight / 18) {
+    for (var x = global.xoffset - player.x; x < global.screenWidth; x += global.screenHeight / 16) {
         graph.moveTo(x, 0);
         graph.lineTo(x, global.screenHeight);
     }
 
-    for (var y = global.yoffset - player.y; y < global.screenHeight; y += global.screenHeight / 18) {
+    for (var y = global.yoffset - player.y; y < global.screenHeight; y += global.screenHeight / 16) {
         graph.moveTo(0, y);
         graph.lineTo(global.screenWidth, y);
     }
