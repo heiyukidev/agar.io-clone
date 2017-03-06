bce7a244f321c933c66675dc76882de10ceb77fb
Khaled Romdhane <heiyukidev@gmail.com>
2017-03-06 11:45:37 +0100

    bundled vendors

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 var path = require('path');
 var webpack = require('webpack');
 module.exports = {
-    entry: './src/client/js/app.js',
+    entry: {
+        app: './src/client/js/app.js'
+    },
+
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'bin/client/js')
