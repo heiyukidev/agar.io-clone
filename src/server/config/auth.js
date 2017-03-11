0cb3d63daff1d65aa4f704f8fc9025eb8f91bffd
Khaled Romdhane <heiyukidev@gmail.com>
2017-03-11 13:51:47 +0100

    final push hopefully

--- src/server/config/auth.js
+++ src/server/config/auth.js
@@ -4,8 +4,8 @@
 module.exports = {
 
     'facebookAuth': {
-        'clientID': '1849476105327877', // your App ID
-        'clientSecret': '92fa52f8dc32f2610ddeb6e3c5d5a4d9', // your App Secret
+        'clientID': '638610069655704', // your App ID
+        'clientSecret': '14b611b171a41c3f3558433073fbc738', // your App Secret
         'callbackURL': 'http://localhost:3000/auth/callback',
         'productionCallbackURL': 'http://elkoura-3andek.tn/auth/callback',
         'profileURL': 'https://graph.facebook.com/v2.8/me?fields=first_name,last_name,email'
